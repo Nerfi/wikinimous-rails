018bdc48fae2fec1b1847d4a4c01091569ebc66a
Juan Paredes <juanchuu.jfpc@gmail.com>
2018-12-26 11:18:53 +0100

    adding routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,24 @@ class ArticlesController < ApplicationController
     @articles = Article.all
 
   end
+
+  def new
+    @articles = Article.new
+
+  end
+
+
+  def create
+
+    @article = Article.new(articles_params)
+    @article.save
+
+    redirect_to articles_path
+
+  end
+
+  def show
+    @article = Article.find(params[:id])
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
   #resources :articles, only: [:create, :index, :destroy]
-  resources :artilces
-  #get 'articles', to: 'articles#index'
+ # resources :artilces
+  get 'articles', to: 'articles#index'
+
+  #addign a new article
+
+  get 'articles/new', to: 'articles#new', as: :new
+  post 'articles', to: 'articles#create'
+
+  #showing one specific article
+   get 'articles/:id', to: 'articles#show', as: :article
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
