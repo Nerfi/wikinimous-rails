5c2815efadbce5e0915d8abf452b0d3e263c21c2
Juan Paredes <juanchuu.jfpc@gmail.com>
2018-12-27 16:27:45 +0100

    before_action not working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -26,13 +27,13 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+   # @article = Article.find(params[:id])
 
   end
 
   def update
 
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     @article.update(article_params)
     #redirect the user to the index
     redirect_to articles_path(@article)
@@ -40,7 +41,7 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     @article.destroy
 
     redirect_to articles_path
@@ -54,6 +55,14 @@ class ArticlesController < ApplicationController
     params.require(:article).permit(:title,:content)
 
   end
+#usamos esto ya que 'show', 'edit', 'update', 'destroy' usan lo mismo
+#'Article.find(params[:id])' also know as DRY
+  def set_article
+    @article = Article.find(params[:id])
+
+  end
 
 
 end
+
+#  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
